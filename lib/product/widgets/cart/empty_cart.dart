572a382d20cf26034e92fbcc05d8383d84b4dbdb
berkeenesaktumen <berkenesaktumen@gmail.com>
2024-06-13 20:20:41 +0300

    Refactor EmptyCart widget to use padding and center alignment

--- lib/product/widgets/cart/empty_cart.dart
+++ lib/product/widgets/cart/empty_cart.dart
@@ -10,15 +10,20 @@ class EmptyCart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.shopping_cart, size: 100),
-          SizedBox(height: 10),
-          Text(StringConstants.emptyCart, style: TextStyle(fontSize: 20)),
-          SizedBox(height: 10),
-          Text(StringConstants.emptyCartSubtitle),
-        ],
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.shopping_cart, size: 100),
+            SizedBox(height: 10),
+            Text(StringConstants.emptyCart, style: TextStyle(fontSize: 20)),
+            SizedBox(height: 10),
+            Text(StringConstants.emptyCartSubtitle,
+                textAlign: TextAlign.center),
+          ],
+        ),
       ),
     );
   }
